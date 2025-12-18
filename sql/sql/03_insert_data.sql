21903c8e0fbf7abb9cfe9286b470907028070a5e
Arshan011 <arsha8923@gmail.com>
2025-12-18 16:20:23 +0530

    Create 03_insert_data.sql

---
+++ sql/sql/03_insert_data.sql
@@ -0,0 +1,25 @@
+INSERT INTO Department VALUES
+(1,'HR'),
+(2,'Sales'),
+(3,'IT');
+
+INSERT INTO Employee VALUES
+(101,'Amit',50000,1),
+(102,'Riya',60000,2),
+(103,'Rahul',55000,3);
+
+INSERT INTO Customer VALUES
+(1,'PharmaCare','Delhi'),
+(2,'HealthPlus','Mumbai');
+
+INSERT INTO Product VALUES
+(1,'Tablet A',120),
+(2,'Syrup B',90);
+
+INSERT INTO Orders VALUES
+(1001,1,'2025-12-01'),
+(1002,2,'2025-12-02');
+
+INSERT INTO Order_Details VALUES
+(1001,1,10),
+(1002,2,5);
